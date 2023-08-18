619ba21eb2f83f4f69a1e994ba80fd082c1ad94c
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-08-18 14:20:10 -0700

    Add title and description to create-arrangement form

--- components/Arrange.jsx
+++ components/Arrange.jsx
@@ -9,6 +9,51 @@ const Arrange = ({ arrangement, setArrangement, submitting, handleSubmit }) => {
           Cook
         </span>
       </h1>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-8 w-full max-w-xs sm:max-w-sm"
+      >
+        <div className="mb-4 flex flex-col rounded-sm bg-slate-900 px-3 pb-3 pt-1.5">
+          <label
+            htmlFor="title"
+            className="mb-1.5 text-left text-xs opacity-50"
+          >
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            value={arrangement.title}
+            maxLength={32}
+            onChange={(e) =>
+              setArrangement({ ...arrangement, title: e.target.value })
+            }
+            placeholder="Your arrangement's name ~"
+            className="w-full bg-slate-900 px-3 text-base outline-none sm:text-lg"
+          />
+        </div>
+        <div className="mb-4 flex flex-col rounded-sm bg-slate-900 px-3 pb-3 pt-1.5">
+          <label
+            htmlFor="description"
+            className="mb-1.5 text-left text-xs opacity-50"
+          >
+            Description
+          </label>
+          <textarea
+            type="text"
+            id="description"
+            value={arrangement.description}
+            rows={6}
+            maxLength={160}
+            onChange={(e) =>
+              setArrangement({ ...arrangement, description: e.target.value })
+            }
+            placeholder="A quick overview ~"
+            className="w-full resize-none bg-slate-900 px-3 text-base outline-none sm:text-lg"
+          />
+        </div>
+      </form>
     </section>
   );
 };
