bf79766362674ee14a36650b5208bc2b7b12f9bd
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-08-18 18:38:46 -0700

    Add timestamps: true

--- models/arrangement.js
+++ models/arrangement.js
@@ -1,14 +1,20 @@
 import { Schema, model, models } from "mongoose";
 
-const arrangementSchema = new Schema({
-  creator: { type: Schema.Types.ObjectId, ref: "User" },
-  title: { type: String, required: [true, "A title is required."] },
-  description: { type: String, required: [true, "A description is required."] },
-  instruments: {
-    type: [String],
-    required: [true, "Instruments are required."],
+const arrangementSchema = new Schema(
+  {
+    creator: { type: Schema.Types.ObjectId, ref: "User" },
+    title: { type: String, required: [true, "A title is required."] },
+    description: {
+      type: String,
+      required: [true, "A description is required."],
+    },
+    instruments: {
+      type: [String],
+      required: [true, "Instruments are required."],
+    },
   },
-});
+  { timestamps: true },
+);
 
 const Arrangement =
   models.Arrangement || model("Arrangement", arrangementSchema);
--- models/user.js
+++ models/user.js
@@ -1,17 +1,21 @@
+import { Timestamp } from "mongodb";
 import { Schema, model, models } from "mongoose";
 
-const userSchema = new Schema({
-  id: {
-    type: String,
-    unique: [true, "This id already exists."],
-    required: [true, "An id is requried."],
+const userSchema = new Schema(
+  {
+    id: {
+      type: String,
+      unique: [true, "This id already exists."],
+      required: [true, "An id is requried."],
+    },
+    username: {
+      type: String,
+      unique: [true, "This username already exists."],
+      required: [true, "A username is required."],
+    },
   },
-  username: {
-    type: String,
-    unique: [true, "This username already exists."],
-    required: [true, "A username is required."],
-  },
-});
+  { timestamps: true },
+);
 
 const User = models.User || model("User", userSchema);
 
