1472a02e093a687fbbafb3eeca7f84df78a45deb
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-11-02 22:53:13 -0700

    Comment out deprecated options from mongoose connection

--- utils/database.js
+++ utils/database.js
@@ -13,8 +13,8 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI, {
       dbName: process.env.DATABASE_NAME,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     isConnected = true;
     console.log("Connected to database.");
