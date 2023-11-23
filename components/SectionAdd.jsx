e8b7dcc9be6548542b259cdc3c236a1acae6cf24
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-11-22 17:04:31 -0800

    Remove animations on disabled buttons

--- components/SectionAdd.jsx
+++ components/SectionAdd.jsx
@@ -6,7 +6,9 @@ const SectionButton = ({ onClick, disabled }) => {
       type="button"
       value="Add Section"
       onClick={onClick}
-      className="mx-auto mb-4 flex w-5/6 items-center justify-center bg-slate-950 bg-opacity-30 py-0.5 shadow-md shadow-slate-950/30 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:py-2"
+      className={`mx-auto mb-4 flex w-5/6 items-center justify-center bg-slate-950 bg-opacity-30 py-0.5 shadow-md shadow-slate-950/30 transition duration-75 ${
+        !disabled && "active:translate-y-0.5 active:scale-95 active:shadow-none"
+      } sm:py-2`}
       disabled={disabled}
     >
       <Plus className="w-4 stroke-slate-100 sm:w-6" />
--- components/SectionDelete.jsx
+++ components/SectionDelete.jsx
@@ -6,7 +6,9 @@ const SectionButton = ({ onClick, disabled }) => {
       type="button"
       value="Delete Section"
       onClick={onClick}
-      className="col-span-1 flex items-center justify-center bg-slate-950 bg-opacity-30 shadow-sm shadow-slate-950/30 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
+      className={`col-span-1 flex items-center justify-center bg-slate-950 bg-opacity-30 shadow-sm shadow-slate-950/30 transition duration-75 ${
+        !disabled && "active:translate-y-0.5 active:scale-95 active:shadow-none"
+      }`}
       disabled={disabled}
     >
       <Trash className="w-4 stroke-slate-100 sm:w-5" />
