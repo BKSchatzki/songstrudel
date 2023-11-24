6d073c49a50ca3fe301898b2fa156303d7131a9d
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-11-24 11:52:59 -0800

    Remove requirement for description and notes in model

--- models/arrangement.js
+++ models/arrangement.js
@@ -4,10 +4,7 @@ const arrangementSchema = new Schema(
   {
     creator: { type: Schema.Types.ObjectId, ref: "User" },
     title: { type: String, required: [true, "A title is required."] },
-    description: {
-      type: String,
-      required: [true, "A description is required."],
-    },
+    description: { type: String },
     instruments: {
       type: [String],
       required: [true, "Instruments are required."],
@@ -15,7 +12,7 @@ const arrangementSchema = new Schema(
     sections: [
       {
         name: { type: String, required: [true, "A name is required."] },
-        notes: { type: String, required: [true, "Notes are required."] },
+        notes: { type: String },
         rows: { type: [[Number]], required: [true, "Cells are required."] },
       },
     ],
