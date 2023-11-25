d5d310719373a0aa6bf4a36cf4dc02d443704fa1
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-11-24 18:30:49 -0800

    Add username to Arrangement view

--- components/ArrangementTitle.jsx
+++ components/ArrangementTitle.jsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 const ArrangementTitle = ({
   arrangement,
   setArrangement,
@@ -5,8 +9,15 @@ const ArrangementTitle = ({
   isNewArrangement,
   disabled,
 }) => {
+  const { data: session } = useSession();
+
   return (
     <label className="mb-4 flex flex-col rounded-sm bg-slate-950 bg-opacity-50 shadow-md shadow-slate-950/50 backdrop-blur-md backdrop-filter focus-within:brightness-150">
+      <span className="absolute -top-2 right-3 rounded-sm bg-slate-950 px-1 py-0.5 text-[0.625rem] opacity-50 backdrop-blur-md sm:text-xs">
+        {arrangement.creator
+          ? arrangement.creator.username
+          : session?.user.name ?? "Anonymous"}
+      </span>
       <span className="px-3 py-1.5 text-left text-xs font-semibold sm:text-base">
         Title
       </span>
