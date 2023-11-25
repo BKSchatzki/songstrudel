657c567813840b020f7708d04a7076fb98f1f4a7
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-11-24 19:04:39 -0800

    Complete UI for my-arrangements management panel

--- components/FeedCard.jsx
+++ components/FeedCard.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const FeedCard = ({ arrangement, isPersonalFeed }) => {
   return (
-    <div className="col-span-1 flex flex-col gap-2">
+    <div className="col-span-1">
       <Link href={`/view-arrangement/${arrangement._id}`}>
         <article className="relative cursor-pointer rounded-sm bg-slate-950 bg-opacity-20 px-8 py-3 shadow-md shadow-slate-950/20 backdrop-blur-md transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none">
           <span className="absolute -top-2 right-3 rounded-sm bg-slate-950 px-1 py-0.5 text-[0.625rem] opacity-50 backdrop-blur-md md:text-xs">
@@ -24,11 +24,11 @@ const FeedCard = ({ arrangement, isPersonalFeed }) => {
         </article>
       </Link>
       {isPersonalFeed && (
-        <div className="rounded-sm bg-slate-800 bg-opacity-20 px-8 py-3 shadow-md shadow-slate-800/20 backdrop-blur-md">
-          <div className="flex w-full items-center justify-center gap-3 text-xs md:text-sm">
+        <div className="rounded-sm bg-slate-800 bg-opacity-20 px-3 py-1.5 shadow-md shadow-slate-800/20 backdrop-blur-md">
+          <div className="flex w-full items-center justify-evenly gap-1 text-xs sm:gap-3 md:text-sm">
             <label
               htmlFor="private"
-              className="flex items-center justify-center gap-1"
+              className="flex items-center justify-center gap-1 text-secondary sm:gap-2"
             >
               <span className="block">Private</span>
               <input
@@ -39,7 +39,7 @@ const FeedCard = ({ arrangement, isPersonalFeed }) => {
             </label>
             <label
               htmlFor="listed"
-              className="flex items-center justify-center gap-1"
+              className="flex items-center justify-center gap-1 text-success sm:gap-2"
             >
               <span className="block">Listed</span>
               <input
@@ -50,8 +50,10 @@ const FeedCard = ({ arrangement, isPersonalFeed }) => {
             </label>
             <button
               type="button"
-              onClick={""}
-              className={`w-[7.5rem] rounded-sm bg-gradient-to-r from-rose-600 to-orange-600 px-3 py-1.5 text-sm font-semibold shadow-sm shadow-red-600 ring-1 ring-red-600 ring-offset-0 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:w-36 sm:text-base`}
+              onClick={() => {
+                return;
+              }}
+              className={`w-24 rounded-sm bg-gradient-to-r from-rose-600 to-orange-600 px-3 py-1.5 font-semibold shadow-sm shadow-red-600 ring-1 ring-red-600 ring-offset-0 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:w-[6.5rem]`}
             >
               Delete
             </button>
