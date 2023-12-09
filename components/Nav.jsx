296dda0a4fdaf0146373f1b298772abe1d530c8c
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-12-09 12:55:01 -0800

    Add animations to Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,10 @@
 "use client";
 
-import Link from "next/link";
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { motion, AnimatePresence } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
 
 const Nav = () => {
   const { data: session } = useSession();
@@ -22,149 +23,208 @@ const Nav = () => {
     <nav className="fixed z-10 w-full bg-gradient-to-b from-gray-900 to-transparent px-6 pb-3 pt-3">
       <div className="items-between mx-auto flex max-w-7xl justify-between">
         <div className="flex items-center justify-center gap-5">
-          <Link
-            href="/"
-            onClick={() => setDropdownToggled(false)}
-            className="flex items-center justify-center gap-2"
+          <motion.div initial={{ y: -100 }} animate={{ y: 0 }}>
+            <Link
+              href="/"
+              onClick={() => setDropdownToggled(false)}
+              className="flex items-center justify-center gap-2"
+            >
+              <Image
+                src="/assets/images/songstrudel-logoonly.svg"
+                alt="SongStrudel Logo"
+                width={37}
+                height={37}
+                className="z-20 block transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none md:hidden"
+              />
+              <Image
+                src="/assets/images/songstrudel-withname.svg"
+                alt="SongStrudel Logo"
+                width={148}
+                height={37}
+                className="z-20 hidden transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none md:block"
+              />
+            </Link>
+          </motion.div>
+          <motion.div
+            initial={{ y: -50 }}
+            animate={{ y: 0, transition: { delay: 0.1 } }}
           >
-            <Image
-              src="/assets/images/songstrudel-logoonly.svg"
-              alt="SongStrudel Logo"
-              width={37}
-              height={37}
-              className="z-20 block transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none md:hidden"
-            />
-            <Image
-              src="/assets/images/songstrudel-withname.svg"
-              alt="SongStrudel Logo"
-              width={148}
-              height={37}
-              className="z-20 hidden transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none md:block"
-            />
-          </Link>
-          <Link
-            href="https://www.buymeacoffee.com/bkschatzki"
-            target="_blank"
-            className="hidden md:block"
-          >
-            <Image
-              src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png"
-              alt="Buy Me A Coffee"
-              width={109}
-              height={30}
-              className="shadow-[#fd605f] transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
-            />
-          </Link>
+            <Link
+              href="https://www.buymeacoffee.com/bkschatzki"
+              target="_blank"
+              className="hidden md:block"
+            >
+              <Image
+                src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png"
+                alt="Buy Me A Coffee"
+                width={109}
+                height={30}
+                className="shadow-[#fd605f] transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
+              />
+            </Link>
+          </motion.div>
         </div>
         {/* Desktop Nav */}
-        <div className="hidden md:flex">
-          {session?.user ? (
-            <div className="flex gap-3 md:gap-5">
-              <Link
-                href="/create-arrangement"
-                className="rounded-sm bg-gradient-to-r from-blue-500 to-violet-500 px-3 py-1.5 font-semibold shadow-sm shadow-indigo-500/50 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
-              >
-                Create Arrangement
-              </Link>
-              <button
-                type="button"
-                onClick={signOut}
-                className=" rounded-sm px-3 py-1.5"
-              >
-                Sign Out
-              </button>
-              <Link href="/my-arrangements">
-                <Image
-                  src={session?.user.image}
-                  alt="My Arrangements"
-                  width={37}
-                  height={37}
-                  className="rounded-full transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
-                />
-              </Link>
-            </div>
-          ) : (
-            <>
-              {providers &&
-                Object.values(providers).map((provider) => (
-                  <button
-                    type="button"
-                    key={provider.name}
-                    onClick={() => signIn(provider.id)}
-                    className="flex items-center justify-center gap-2 rounded-sm bg-[#4752c4] px-3 py-1.5 font-semibold text-slate-100 shadow-sm shadow-[#4752c4] transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
+        <AnimatePresence mode="wait">
+          <div className="hidden md:flex">
+            {session?.user ? (
+              <div className="flex gap-3 md:gap-5">
+                <motion.div
+                  className="flex items-center justify-center"
+                  initial={{ y: -50 }}
+                  animate={{ y: 0, transition: { delay: 0.2 } }}
+                >
+                  <Link
+                    href="/create-arrangement"
+                    className="rounded-sm bg-gradient-to-r from-blue-500 to-violet-500 px-3 py-1.5 font-semibold shadow-sm shadow-indigo-500/50 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
                   >
+                    Create Arrangement
+                  </Link>
+                </motion.div>
+                <motion.button
+                  type="button"
+                  onClick={signOut}
+                  className=" rounded-sm px-3 py-1.5"
+                  initial={{ y: -100 }}
+                  animate={{ y: 0, transition: { delay: 0.1 } }}
+                >
+                  Sign Out
+                </motion.button>
+                <motion.div initial={{ y: -150 }} animate={{ y: 0 }}>
+                  <Link href="/my-arrangements">
                     <Image
-                      src="/assets/images/discord-logo.svg"
-                      alt="Discord Logo"
-                      width={25}
-                      height={25}
+                      src={session?.user.image}
+                      alt="My Arrangements"
+                      width={37}
+                      height={37}
+                      className="rounded-full transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
                     />
-                    Sign In
-                  </button>
-                ))}
-            </>
-          )}
-        </div>
+                  </Link>
+                </motion.div>
+              </div>
+            ) : (
+              <>
+                {providers &&
+                  Object.values(providers).map((provider) => (
+                    <motion.button
+                      type="button"
+                      key={provider.name}
+                      onClick={() => signIn(provider.id)}
+                      className="flex items-center justify-center gap-2 rounded-sm bg-[#4752c4] px-3 py-1.5 font-semibold text-slate-100 shadow-sm shadow-[#4752c4] transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
+                      initial={{ y: -150 }}
+                      animate={{ y: 0 }}
+                      exit={{
+                        opacity: 0,
+                        transition: { duration: 0.1 },
+                      }}
+                    >
+                      <Image
+                        src="/assets/images/discord-logo.svg"
+                        alt="Discord Logo"
+                        width={25}
+                        height={25}
+                      />
+                      Sign In
+                    </motion.button>
+                  ))}
+              </>
+            )}
+          </div>
+        </AnimatePresence>
         {/* Mobile Nav */}
         <div className="flex md:hidden">
           {session?.user ? (
             <div className="flex">
-              <Image
-                src={session?.user.image}
-                alt="My Discord Avatar"
-                width={37}
-                height={37}
-                onClick={() => setDropdownToggled((prev) => !prev)}
-                className="z-20 rounded-full transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
-              />
-              {dropdownToggled && (
-                <div className="fixed right-0 top-0 z-10 flex h-full w-full flex-col items-center gap-6 rounded-sm bg-gray-900 bg-opacity-20 px-4 py-4 pt-32 text-right backdrop-blur-md backdrop-filter">
-                  <Link
-                    href="/my-arrangements"
-                    onClick={() => setDropdownToggled(false)}
-                    className="w-3/4 max-w-xs rounded-sm bg-gradient-to-r from-orange-400 to-yellow-400 px-3 py-1.5 text-center text-sm font-semibold text-slate-950 shadow-sm shadow-amber-400/50 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:text-base"
+              <motion.div initial={{ y: -150 }} animate={{ y: 0 }}>
+                <Image
+                  src={session?.user.image}
+                  alt="My Discord Avatar"
+                  width={37}
+                  height={37}
+                  onClick={() => setDropdownToggled((prev) => !prev)}
+                  className="z-20 rounded-full transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
+                />
+              </motion.div>
+              <AnimatePresence>
+                {dropdownToggled && (
+                  <motion.div
+                    className="fixed right-0 top-0 z-10 flex h-full w-full flex-col items-center gap-6 rounded-sm bg-gray-900 bg-opacity-20 px-4 py-4 pt-32 text-right backdrop-blur-md backdrop-filter"
+                    initial={{ opacity: 0, x: 500 }}
+                    animate={{ opacity: 1, x: 0, zIndex: -10 }}
+                    transition={{ duration: 0.3333, ease: "anticipate" }}
+                    exit={{
+                      opacity: 0,
+                      x: 500,
+                      transition: { delay: 0.25 },
+                    }}
                   >
-                    My Arrangements
-                  </Link>
-                  <Link
-                    href="/create-arrangement"
-                    onClick={() => setDropdownToggled(false)}
-                    className="w-3/4 max-w-xs rounded-sm bg-gradient-to-r from-blue-500 to-violet-500 px-3 py-1.5 text-center text-sm font-semibold shadow-sm shadow-indigo-500/50 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:text-base"
-                  >
-                    Create Arrangement
-                  </Link>
-                  <button
-                    type="button"
-                    onClick={signOut}
-                    className="mt-14 w-3/4 max-w-xs rounded-sm bg-slate-100 px-3 py-1.5 text-sm text-slate-950 shadow-sm shadow-slate-100/50 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:text-base"
-                  >
-                    Sign Out
-                  </button>
-                  <Link
-                    href="https://www.buymeacoffee.com/bkschatzki"
-                    target="_blank"
-                    className="fixed left-20 top-[15px]"
-                  >
-                    <Image
-                      src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png"
-                      alt="Buy Me A Coffee"
-                      width={109}
-                      height={30}
-                      className="shadow-[#fd605f] transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
-                    />
-                  </Link>
-                </div>
-              )}
+                    <Link
+                      href="/my-arrangements"
+                      onClick={() => setDropdownToggled(false)}
+                      className="w-3/4 max-w-xs rounded-sm bg-gradient-to-r from-orange-400 to-yellow-400 px-3 py-1.5 text-center text-sm font-semibold text-slate-950 shadow-sm shadow-amber-400/50 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:text-base"
+                    >
+                      My Arrangements
+                    </Link>
+                    <Link
+                      href="/create-arrangement"
+                      onClick={() => setDropdownToggled(false)}
+                      className="w-3/4 max-w-xs rounded-sm bg-gradient-to-r from-blue-500 to-violet-500 px-3 py-1.5 text-center text-sm font-semibold shadow-sm shadow-indigo-500/50 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:text-base"
+                    >
+                      Create Arrangement
+                    </Link>
+                    <button
+                      type="button"
+                      onClick={signOut}
+                      className="mt-14 w-3/4 max-w-xs rounded-sm bg-slate-100 px-3 py-1.5 text-sm text-slate-950 shadow-sm shadow-slate-100/50 transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:text-base"
+                    >
+                      Sign Out
+                    </button>
+                    <motion.div
+                      className="fixed left-20 top-[15px]"
+                      initial={{ opacity: 0, y: -150 }}
+                      animate={{
+                        opacity: 1,
+                        y: 0,
+                        transition: { delay: 0.25 },
+                      }}
+                      exit={{
+                        opacity: 0,
+                        y: -150,
+                      }}
+                    >
+                      <Link
+                        href="https://www.buymeacoffee.com/bkschatzki"
+                        target="_blank"
+                        // className="fixed left-20 top-[15px]"
+                      >
+                        <Image
+                          src="https://cdn.buymeacoffee.com/buttons/v2/default-red.png"
+                          alt="Buy Me A Coffee"
+                          width={109}
+                          height={30}
+                          className="shadow-[#fd605f] transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none"
+                        />
+                      </Link>
+                    </motion.div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </div>
           ) : (
             <>
               {providers &&
                 Object.values(providers).map((provider) => (
-                  <button
+                  <motion.button
                     type="button"
                     key={provider.name}
                     onClick={() => signIn(provider.id)}
                     className="flex items-center justify-center gap-2 rounded-sm bg-[#4752c4] px-3 py-1.5 text-sm font-semibold text-slate-100 shadow-sm shadow-[#4752c4] transition duration-75 active:translate-y-0.5 active:scale-95 active:shadow-none sm:text-base"
+                    initial={{ y: -150 }}
+                    animate={{ y: 0 }}
+                    exit={{
+                      opacity: 0,
+                      transition: { duration: 0.1 },
+                    }}
                   >
                     <Image
                       src="/assets/images/discord-logo.svg"
@@ -173,7 +233,7 @@ const Nav = () => {
                       height={25}
                     />
                     Sign In
-                  </button>
+                  </motion.button>
                 ))}
             </>
           )}
